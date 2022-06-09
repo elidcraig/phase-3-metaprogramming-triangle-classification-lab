5a99629ce20ecbd5a3b218eb0e52812f56da25f5
Eli Craig <elidcraig@gmail.com>
2022-06-09 11:01:22 -0700

    commit tree 5d265319c8ec482e57235ff80ec7980d186cc728

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :sides
+
+  def initialize side1, side2, side3
+    @sides = [side1, side2, side3]
+  end
+
+  def kind
+    if @sides.include?(0)
+      TriangleError
+    elsif sides.uniq.length == 1
+      :equilateral
+    elsif sides.uniq.length == 2
+      :isosceles
+    elsif sides.uniq.length == 3
+      :scalene  
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
+
+t1 = Triangle.new(2,3,3)
+t2 = Triangle.new(5,5,5)
+t3 = Triangle.new(6, 7, 8)
+
+def reload
+  load 'lib/triangle.rb'
+end
+
+# binding.pry
